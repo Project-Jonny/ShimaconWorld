56638f0a281598411684aae1a83b77018dbaa280
Jonny <gueesindango@gmail.com>
2020-12-19 01:20:17 +0900

    add sprite

---
+++ New Unity Project/Assets/Resource/Scripts/Manager/DragManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragManager : MonoBehaviour
+{
+    public GameObject[] drags;
+
+    void Start()
+    {
+        
+    }
+
+    IEnumerator InstDrag()
+    {
+        yield return null;
+
+        while (true)
+        {
+            int r = Random.Range(0, drags.Length);
+            drags[r].GetComponent<EnemySpawn>().Spawn();
+        }
+    }
+}
