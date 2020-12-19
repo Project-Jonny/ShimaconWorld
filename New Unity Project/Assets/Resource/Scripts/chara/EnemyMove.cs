185066ed12c0480ff0e312995594575453985c81
嶋津恒彦 <simanezumi1989@gmail.com>
2020-12-20 03:19:06 +0900

    敵の挙動と発射方向の修正

--- New Unity Project/Assets/Resource/Scripts/chara/EnemyMove.cs
+++ New Unity Project/Assets/Resource/Scripts/chara/EnemyMove.cs
@@ -28,41 +28,49 @@ public class EnemyMove : MonoBehaviour
         {
             target = GameObject.FindGameObjectWithTag("Bear").GetComponent<Chase>();
             gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
-            StartCoroutine(RandomMove());
+            StartCoroutine(Move());
             StartCoroutine(Shooting());
         }
     }
 
-    IEnumerator RandomMove()
+    IEnumerator Move()
     {
         while (true)
         {
             yield return null;
 
+            //ぷえんモード
             while (target.emotional == false)
             {
-                yield return new WaitForSeconds(1);
-                int r = Random.Range(0, 4);
-                switch (r)
-                {
-                    case 0:
-                        direction = Vector2.up;
-                        break;
-                    case 1:
-                        direction = Vector2.down;
-                        break;
-                    case 2:
-                        direction = Vector2.left;
-                        break;
-                    case 3:
-                        direction = Vector2.right;
-                        break;
-                }
-                rb.velocity = direction;
-                animator.SetFloat("Direction_X", direction.x);
-                animator.SetFloat("Direction_Y", direction.y);
+                yield return RandomMove();
             }
+            //ぷえんモードを抜けると速度を0にする
+            rb.velocity = Vector3.zero;
         }
+    }
+
+    IEnumerator RandomMove()
+    {
+        yield return new WaitForSeconds(1);
+        int r = Random.Range(0, 4);
+        switch (r)
+        {
+            case 0:
+                direction = Vector2.up;
+                break;
+            case 1:
+                direction = Vector2.down;
+                break;
+            case 2:
+                direction = Vector2.left;
+                break;
+            case 3:
+                direction = Vector2.right;
+                break;
+        }
+        rb.velocity = direction;
+        animator.SetFloat("Direction_X", direction.x);
+        animator.SetFloat("Direction_Y", direction.y);
     }
 
     IEnumerator Shooting()
@@ -98,8 +106,30 @@ public class EnemyMove : MonoBehaviour
                 transform.position = Vector2.MoveTowards(transform.position, target.transform.position, 1 * Time.deltaTime);
                 animator.SetFloat("Direction_X", target.transform.position.x - transform.position.x);
                 animator.SetFloat("Direction_Y", target.transform.position.y - transform.position.y);
+                direction = Normalization(target.transform.position, transform.position); // 一旦向きを入れる:ここから発射方向を決める
             }
         }
+    }
+
+    // 斜め方向を上下左右に変換する
+    Vector2 Normalization(Vector2 target, Vector2 self)
+    {
+        Vector2 dir = target - self; // 進行方向を取得する
+        dir.Normalize();             // 長さを1にする
+        // 4方向でもっとも近いものを取得する
+        Vector2[] around = new Vector2[4] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+        Vector2 minVec = around[0];
+        float minDis = Vector2.Distance(dir, minVec);
+        for (int i=1; i<around.Length; i++)
+        {
+            if (Vector2.Distance(dir, around[i]) < minDis)
+            {
+                // minDisより小さいなら最小を入れ替える
+                minVec = around[i];
+                minDis = Vector2.Distance(dir, minVec);
+            }
+        }
+        return minVec;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
